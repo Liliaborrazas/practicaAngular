b848fb1f7b6a541288fea5df9f26957866952f1b
lilia14-admin <lilia.borrazas@babelgroup.com>
2023-10-25 12:55:11 +0200

    creo servicios, paso datos con el input y el output

--- src/app/dbz/components/list/list.component.ts
+++ src/app/dbz/components/list/list.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Character } from '../../interfaces/character.interface';
 
 @Component({
@@ -7,10 +7,22 @@ import { Character } from '../../interfaces/character.interface';
   styleUrls: ['./list.component.css']
 })
 export class ListComponent {
+
   @Input()
   public characterList: Character[] = [{
     name: 'Trunk',
     power: 10
   }]
+
+  //onDeleted(index value : number)
+  @Output()
+  public onDeleted : EventEmitter<string> = new EventEmitter();
+
+
+  onDeletedCharacter(id:string):void {
+    this.onDeleted.emit(id)
+    // Emitir el id del personaje
+    console.log(id)
+  }
 
 }
